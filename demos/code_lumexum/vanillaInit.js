c1617d9b5575ed82d49759195f6c072307e8b162
Matěj Suchánek <kkartk322@gmail.com>
2022-02-13 22:36:33 +0000

    code_lumexum init commit

---
+++ demos/code_lumexum/vanillaInit.js
@@ -0,0 +1,26 @@
+// import { TangleDevice } from "./TangleDevice.js";
+// import { TimeTrack } from "./TimeTrack.js";
+// import { TnglCodeParser } from "./TangleParser.js";
+// import { uint8ArrayToHexString, computeTnglFingerprint } from "./functions.js";
+import TangleMsgBox from "./webcomponents/dialog-component.js";
+// import "../../control.js";
+
+// window.TangleDevice = TangleDevice;
+// window.TimeTrack = TimeTrack;
+// window.TnglCodeParser = TnglCodeParser;
+// window.uint8ArrayToHexString = uint8ArrayToHexString;
+// window.computeTnglFingerprint = computeTnglFingerprint;
+window.TangleMsgBox = TangleMsgBox;
+
+function injectScript(src) {
+  const script = document.createElement("script");
+  script.src = src;
+  document.head.appendChild(script);
+}
+
+injectScript("code.js");
+// injectScript("control.js");
+// Load the Code demo's language strings.
+injectScript('msg/en.js');
+// Load Blockly's language strings.
+injectScript('../../msg/js/en.js');
